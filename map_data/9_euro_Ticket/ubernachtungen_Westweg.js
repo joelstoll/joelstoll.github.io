e7c9891e9eaafde562201c57caa40b658cb541a9
joelstoll <48290123+joelstoll@users.noreply.github.com>
2022-06-30 09:59:36 +0200

    new page start 9euoTicket

---
+++ map_data/9_euro_Ticket/ubernachtungen_Westweg.js
@@ -0,0 +1,35 @@
+var Tour_3_ubernachtungen = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "5 Juni 2022",
+                "time": "2022-06-06T05:50:36.023Z"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    8.120031,
+                    47.94015,
+                    1188
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "4 Juni 2022",
+                "time": "2022-06-05T06:11:36.642Z"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    8.142295,
+                    48.115629,
+                    1147
+                ]
+            }
+        }
+    ]
+}
